c4410e2d7f68ff4093411b73a93fa87a43d324ad
Vijayguguloth <85604285+Vijayguguloth@users.noreply.github.com>
2021-06-10 20:44:19 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath( path.dirname( _file_) )
+
+setup(
+      name='three_Flask'
+      version='1.0.0'
+      description='Python flask app for predicting age and gender using deep learning
+      licence='Apacha-2.0'
+     
+        
+)
+
